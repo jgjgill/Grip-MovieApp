2fd59f135580f534f4a4325d56377f7ad8034722
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-05-13 15:42:48 +0900

    Chore - 불필요한 코드 제거

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 
 import Layout from 'components/Layout'
@@ -13,7 +13,6 @@ const App = () => {
           <Route element={<Layout />}>
             <Route path='/' element={<Home />} />
             <Route path='bookmark' element={<Bookmark />} />
-            <Route path='*' element={<Navigate replace to='/' />} />
           </Route>
         </Routes>
       </BrowserRouter>
